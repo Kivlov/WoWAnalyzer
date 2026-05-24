7371acf3d0335429590b5c3deef6ce0e82ddf2e0
Kivlov <lordkiv@hotmail.com>
2026-05-24 02:27:30 -0300

    Remove 'primed swipe' evaluation

--- src/analysis/retail/hunter/survival/modules/talents/Takedown/TakedownSection.tsx
+++ src/analysis/retail/hunter/survival/modules/talents/Takedown/TakedownSection.tsx
@@ -4,6 +4,7 @@ import Takedown from './Takedown';
 import SpellLink from 'interface/SpellLink';
 import TALENTS from 'common/TALENTS/hunter';
 import SPELLS from 'common/SPELLS/hunter';
+
 import { EventType } from 'parser/core/Events';
 import Explanation from 'interface/guide/components/Explanation';
 import CooldownGrid from 'interface/CooldownGrid/CooldownGrid';
@@ -27,11 +28,6 @@ export default function TakedownSection(): JSX.Element | null {
   return (
     <SubSection title={<SpellLink spell={TALENTS.TAKEDOWN_TALENT} />}>
       <Explanation>
-        <p>
-          Always enter <SpellLink spell={TALENTS.TAKEDOWN_TALENT} /> with a{' '}
-          <SpellLink spell={SPELLS.RAPTOR_SWIPE_BUFF} /> buff active.
-        </p>
-
         {hasTwinFangs ? (
           <p>
             With <SpellLink spell={TALENTS.TWIN_FANGS_TALENT} />, Takedown generates{' '}
@@ -46,12 +42,6 @@ export default function TakedownSection(): JSX.Element | null {
             stacks during Takedown.
           </p>
         )}
-        <p>
-          The analysis below is done as if you are in a situation in which you can follow the APL.
-          Some fights may dictate that you do not prime Swipe, or that you immediately Kill Command
-          to spawn a beast such as the tight timings on Mythic Averzian. In these cases, the
-          suggestions below may not be applicable.
-        </p>
       </Explanation>
       <CooldownGrid
         label={<SpellLink spell={TALENTS.TAKEDOWN_TALENT} />}
